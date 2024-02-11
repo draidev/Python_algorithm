7d1f2689403f37ea2e98a74e1297c90e4fa021f7
JeonJoonyong <drtkdldjstm@naver.com>
2024-02-11 15:09:16 +0900

    update 1012_유기농배추.py : python재귀 limit 늘리는 설정 추가

--- Baekjoon/1012_유기농배추.py
+++ Baekjoon/1012_유기농배추.py
@@ -1,5 +1,8 @@
+import sys
+sys.setrecursionlimit(10**6)
+
 def dfs(adj_matrix, visited, x, y, M, N):
-    if x < 0 or x >= M or y < 0 or y > N or visited[x][y] or adj_matrix[x][y] == 0:
+    if x < 0 or x >= M or y < 0 or y >= N or visited[x][y] or adj_matrix[x][y] == 0:
         return
     
     visited[x][y] = True
@@ -16,11 +19,14 @@ def count_cabbage_worms(adj_matrix, M, N):
         for j in range(N):
             if not visited[i][j] and adj_matrix[i][j] == 1:
                 dfs(adj_matrix, visited, i, j, M ,N)
-                worms_counts += 1
+                worms_count += 1
+
+    return worms_count
 
 def main():
     T = int(input())
 
+    result = list()
     for _ in range(T):
         M, N, K = map(int, input().split())
         adj_matrix = [[0 for _ in range(N)] for _ in range(M)]
@@ -30,7 +36,10 @@ def main():
             adj_matrix[X][Y] = 1
 
         worms_required = count_cabbage_worms(adj_matrix, M, N)
-        print(worms_required)
+        result.append(worms_required)
+
+    for r in result:
+        print(r)
 
 if __name__=='__main__':
     main()
