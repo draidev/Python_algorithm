2ddd62bf1b6ec114fbdfa5988675193971fb3b53
JeonJoonyong <drtkdldjstm@naver.com>
2024-02-15 22:26:24 +0900

    update 7576_토마토.py : add annotation

--- Baekjoon/7576_토마토.py
+++ Baekjoon/7576_토마토.py
@@ -12,6 +12,7 @@ def count_tomato(tomatoes, M, N):
     queue = deque()
     days = 0
     
+    # 덜익은 토마토의 개수를 구해야 한다.
     unripe_count = 0
     for i in range(N):
         for j in range(M):
@@ -20,15 +21,18 @@ def count_tomato(tomatoes, M, N):
             elif tomatoes[i][j] == 0:
                 unripe_count += 1
                 
+
+    # queue에 들어있는 요소가 여러개라면 요소의 개수만큼 반복문을 돌린다.
     while queue:
         size = len(queue)
         for _ in range(size):
             x,y = queue.popleft()
             for dx, dy in directions:
                 nx, ny = x + dx, y + dy
+                # [x][y]배열에서 x는 세로의 길이 N, y는 가로의 길이 M
                 if 0 <= nx and nx < N and 0 <= ny and ny < M and tomatoes[nx][ny] == 0:
                     tomatoes[nx][ny] = 1
-                    unripe_count -= 1
+                    unripe_count -= 1 
                     queue.append((nx, ny))
                     
         days += 1
