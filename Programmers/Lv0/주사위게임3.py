22af39766b3e780cec2611dc2d07d42e57ed05a3
jyJeon <drtkdldjstm@naver.com>
2024-05-10 16:26:34 +0900

    update 주사위게임3.py : Lv0

--- Programmers/Lv0/주사위게임3.py
+++ Programmers/Lv0/주사위게임3.py
@@ -1,56 +1,34 @@
-def solution(a, b, c, d):
-    answer = 0
-    abcd_list = [a,b,c,d]
-    same_list = list()
-    rest_list = list()
-    rest2_list = list()
-    p, q, r = 0, 0, 0
-        
-    for i in abcd_list:
-        if same_list==[]:
-            same_list.append(i)
-        elif i in same_list:
-            same_list.append(i)
-        elif rest_list==[]:
-            rest_list.append(i)
-        elif i in rest_list:
-            rest_list.append(i)
-        elif rest2_list==[]:
-            rest2_list.append(i)
-        else:
-            rest2_list.append(i)
-            
+from collections import Counter
+
+def solution(a, b, c, d):    
+    # Count frequency of each number
+    counts = Counter([a, b, c, d])
+    items = list(counts.items())
+    items.sort(key=lambda x: -x[1])  # Sort by frequency, descending
     
-    if same_list!=[]:
-        p = same_list[0]
-    if rest_list!=[]:
-        q = rest_list[0]
-    if rest2_list!=[]:
-        r = rest2_list[0]
-        
-    a_len = len(same_list)
-    b_len = len(rest_list)
-    c_len = len(rest2_list)
-        
-    print(p,q,r)
-                
-    if a==b==c==d:
-        answer = 1111*a
-        
-    if a_len==3:
-         answer = (10*p+q)**2
-    elif b_len==3:
-        answer = (10*q+p)**2
-    elif c_len==3:
-        answer = (10*r+q)**2
-        
-    if a_len==2 and b_len==2:
-        answer = (p+q) * abs(p-q)
-    elif len(same_list)==2 and len(rest_list)==1:
-        answer = q*r
-    elif len(same_list)==1 and len(rest_list)==2:
-        answer = p*r
-    elif p!=q!=r:
-        answer = min(abcd_list)
+    if len(items) == 1:
+        # All four numbers are the same
+        p = items[0][0]
+        return 1111 * p
+    elif len(items) == 2:
+        if items[0][1] == 3:
+            # Three numbers are p, one is q
+            p = items[0][0]
+            q = items[1][0]
+            return (10 * p + q) ** 2
+        else:
+            # Two numbers are p, two numbers are q
+            p = items[0][0]
+            q = items[1][0]
+            return (p + q) * abs(p - q)
+    elif len(items) == 3:
+        # One number repeats twice, the others are different
+        p = next(x[0] for x in items if x[1] == 2)
+        others = [x[0] for x in items if x[1] == 1]
+        q, r = others[0], others[1]
+        return q * r
+    elif len(items) == 4:
+        # All numbers are different
+        return min(a, b, c, d)
 
     return answer
