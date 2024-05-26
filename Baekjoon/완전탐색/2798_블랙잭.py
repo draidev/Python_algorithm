0c3963693e6cb41e7a7e132a37bc6fa06b8ecd28
draidev <drtkdldjstm@naver.com>
2024-05-26 18:47:48 +0900

    modify 2798_블랙잭.py

--- Baekjoon/완전탐색/2798_블랙잭.py
+++ Baekjoon/완전탐색/2798_블랙잭.py
@@ -5,11 +5,23 @@ N, M = map(int, input().split())
 
 numbers = list(map(int, input().split()))
 
-number_comb = list(itertools.combinations(numbers, 3))
+''' 조합(combination) 사용 '''
+# number_comb = list(itertools.combinations(numbers, 3))
 
-answer = 0
-for i in number_comb:
-    if sum(i) <= M:
-        answer = max(answer, sum(i))
+# answer = 0
+# for i in number_comb:
+#     if sum(i) <= M:
+#         answer = max(answer, sum(i))
     
+# print(answer)
+
+''' for loop 사용 '''
+answer = 0
+for i in range(N):
+    for j in range(i+1, N):
+        for k in range(j+1, N):
+            cur_sum = numbers[i] + numbers[j] + numbers[k]
+            if cur_sum <= M and cur_sum > answer:
+                answer = cur_sum
+                
 print(answer)
