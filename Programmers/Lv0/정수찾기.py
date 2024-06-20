72a1036fc76e38eb119908c7cbbb9ff1f367f0d3
Joonyong Jeon <drtkdldjstm@gmail.com>
2024-06-21 06:00:02 +0900

    # update 정수찾기.py \\ Lv0 \\ https://school.programmers.co.kr/learn/courses/30/lessons/181840

---
+++ Programmers/Lv0/정수찾기.py
@@ -0,0 +1,6 @@
+# update 정수찾기.py \\ Lv0 \\ https://school.programmers.co.kr/learn/courses/30/lessons/181840
+def solution(num_list, n):
+    if n in num_list:
+        return 1
+    else:
+        return 0
