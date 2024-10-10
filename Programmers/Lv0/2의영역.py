e47b3f95d08feafee943cee80f8a9561bcd1f1ad
JeonJoonyong <drtkdldjstm@naver.com>
2024-10-10 23:56:27 +0900

    update 2의 영역.py

---
+++ Programmers/Lv0/2의영역.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    first = 0
+    last = 0
+    for i in range(arr):
+        if arr[i]==2:
+            answer.append(i)
+    
+    print(a)
+    return answer
